450a556564acb2cfdbf4b2d31eaec30f62dc376e
errea <erreakezy@gmail.com>
2021-07-20 22:15:40 +0100

    Add function for calculations

---
+++ calculate.js
@@ -0,0 +1,19 @@
+const calculate = {
+    add(a, b) {
+      return a+b;
+    },
+  
+    subs(a, b) {
+      return a-b;
+    },
+  
+    divide(a, b) {
+      return a/b;
+    },
+  
+    mult(a, b) {
+      return a*b;
+    },
+  }
+
+  module.exports = calculate;
